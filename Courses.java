5389366ab9551b3c4a4e7b1f138b0a12c89dc414
tataotb57-sys <tataotb57@gmail.com>
2026-07-21 17:30:51 +0300

    Create Courses.java

---
+++ Courses.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cyberjocx;
+
+/**
+ *
+ * @author Y_H00
+ */
+public class Courses
+{
+    int id;
+    int price_point;
+    String nmae_of_coures;
+    String instractor;
+    String link;
+public Courses(int id , int price_point, String nmae , String instractor ,String link){
+    this.id=id;
+    this.instractor =   instractor;
+    this.link = link;
+    this.nmae_of_coures=nmae;
+    this.price_point=price_point;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+}
